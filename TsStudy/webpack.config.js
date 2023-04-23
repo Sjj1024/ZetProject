2f672a67e37923d532e6586347714fd45ad81d24
songjj <songjj@zetyun.com>
2023-04-23 10:40:15 +0800

    add:some

--- TsStudy/webpack.config.js
+++ TsStudy/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')//引入内置path方便得到绝对路径
 const HtmlWebpackPlugin = require('html-webpack-plugin')//引入模板组件
-
+// const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'development',//开发模式
@@ -9,19 +9,15 @@ module.exports = {
         path: path.resolve(__dirname, "./dist"),//出口文件，即打包后的文件存放地址
         filename: 'bundle.js' //文件名
     },
-    devServer: {
-
-    },
+    devServer: {},
     resolve: {
-        extensions:['.ts', '.js', '.cjs', '.json'] //配置文件引入时省略后缀名
+        extensions: ['.ts', '.js', '.cjs', '.json'] //配置文件引入时省略后缀名
     },
     module: {
-        rules: [
-            {
-                test: /\.ts$/, //匹配规则 以ts结尾的文件
-                loader: 'ts-loader' //对应文件采用ts-loader进行编译
-            }
-        ]
+        rules: [{
+            test: /\.ts$/, //匹配规则 以ts结尾的文件
+            loader: 'ts-loader' //对应文件采用ts-loader进行编译
+        }]
     },
     plugins: [
         new HtmlWebpackPlugin({
